237da5a4a5bdcf6161d1d8e2c163b0084a104bcd
acalvoe-design <acalvoelizondo@gmail.com>
2026-06-12 10:01:14 -0600

    Create class_SensorTemperatura.py
    Se implementa la clase SensorTemperatura heredada de Sensor, encargada de gestionar la lectura de temperatura y proporcionar información al sistema para la toma de decisiones automáticas.

---
+++ class_SensorTemperatura.py
@@ -0,0 +1,11 @@
+from sensor import Sensor
+
+
+class SensorTemperatura(Sensor):
+    def __init__(self, pin, umbral_min, umbral_max, unidad="°C"):
+        super().__init__(pin, umbral_min, umbral_max)
+        self.unidad = unidad
+
+    def leer(self):
+        print("Leyendo temperatura...")
+        return self.valor_actual
