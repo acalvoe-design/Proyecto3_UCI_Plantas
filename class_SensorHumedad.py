4f870f4dc23e88e41cf1d55dc61943f6b96ee6c6
acalvoe-design <acalvoelizondo@gmail.com>
2026-06-12 10:11:22 -0600

    Create class_SensorHumedad.py
    Se implementa la clase SensorHumedad heredada de Sensor, encargada de gestionar la lectura de humedad y proporcionar información al sistema para la toma de decisiones automáticas.

---
+++ class_SensorHumedad.py
@@ -0,0 +1,11 @@
+from sensor import Sensor
+
+
+class SensorHumedad(Sensor):
+    def __init__(self, pin, umbral_min, umbral_max, tipo="DHT22"):
+        super().__init__(pin, umbral_min, umbral_max)
+        self.tipo = tipo
+
+    def leer(self):
+        print("Leyendo humedad...")
+        return self.valor_actual
