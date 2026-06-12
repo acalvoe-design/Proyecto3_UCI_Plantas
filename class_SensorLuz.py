aae004b9436e7ba73d91c7bc78f201a09a1b41e1
acalvoe-design <acalvoelizondo@gmail.com>
2026-06-12 10:15:11 -0600

    Create class_SensorLuz.py
    Se implementa la clase SensorLuz heredada de Sensor, encargada de gestionar la lectura de iluminación y proporcionar información al sistema para la toma de decisiones automáticas.

---
+++ class_SensorLuz.py
@@ -0,0 +1,11 @@
+from sensor import Sensor
+
+
+class SensorLuz(Sensor):
+    def __init__(self, pin, umbral_min, umbral_max, lux=0):
+        super().__init__(pin, umbral_min, umbral_max)
+        self.lux = lux
+
+    def leer(self):
+        print("Leyendo intensidad lumínica...")
+        return self.valor_actual
