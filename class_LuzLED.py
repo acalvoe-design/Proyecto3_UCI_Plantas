74ce63cf0a7a86cbad8939ceaa4ddc7e53e233e8
acalvoe-design <acalvoelizondo@gmail.com>
2026-06-13 09:53:39 -0600

    Create class_LuzLED.py
    Se implementa la clase LuzLED heredada de Actuador, encargada de gestionar la iluminación del microambiente y proporcionar las acciones necesarias para la regulación automática del entorno de la planta.

---
+++ class_LuzLED.py
@@ -0,0 +1,8 @@
+class LuzLED(Actuador):
+    def activar(self):
+        self.activo = True
+        print("Luz LED encendida")
+
+    def desactivar(self):
+        self.activo = False
+        print("Luz LED apagada")
